9512de5b93ac2888b11ebf286cfdf7c190098545
orson391 <134856006+orson391@users.noreply.github.com>
2023-05-29 13:15:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 console.log("Welcome To Recipes");
 
+window.addEventListener('DOMContentLoaded', function() {
+    var preloader = document.getElementById('preloader');
+    setTimeout(function() {
+      preloader.style.transition = 'opacity 0.3s';
+      preloader.style.opacity = 0;
+      setTimeout(function() {
+        preloader.style.display = 'none';
+      }, 3000); // Change 3000 to the desired duration in milliseconds
+    }, 3000); // Add a delay to ensure the preloader is displayed for a minimum duration
+  });
