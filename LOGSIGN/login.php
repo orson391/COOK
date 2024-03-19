ba63b08bb6bf034248b201ecd59c217a5d09d044
orson391 <newtham29@gmail.com>
2024-03-19 20:07:57 +0530

    yess

---
+++ LOGSIGN/login.php
@@ -0,0 +1,36 @@
+<?php
+include('../DATABASE/db.php');
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username']; 
+    $password = $_POST['password']; // Get the password from the form
+
+    $sql = "SELECT * FROM users WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {   
+            
+
+            $_SESSION['username'] = $username; 
+            
+
+            header("Location: /COOK/MENU/index.html");
+            exit;
+        } else {
+            echo "Incorrect password!";
+        }
+    } else {
+        echo "User not found!";
+    }
+
+    $stmt->close(); // Close the statement
+    $conn->close(); // Close the database connection
+}
+?>
---
+++ LOGSIGN/signup.php
@@ -0,0 +1,41 @@
+<?php
+include('../DATABASE/db.php');
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username']; 
+    $email = $_POST['email']; 
+    $password = $_POST['password']; // Get the password from the form
+    
+   
+    // Check if the username is already taken
+    $username_check_sql = "SELECT * FROM users WHERE username = ?";
+    $username_check_stmt = $conn->prepare($username_check_sql);
+    $username_check_stmt->bind_param("s", $username);
+    $username_check_stmt->execute();
+
+    $username_check_result = $username_check_stmt->get_result();
+
+    if ($username_check_result->num_rows > 0) {
+        echo "Username already exists!";
+        exit; // Stop execution if the username is taken
+    }
+
+    // Use prepared statements to insert user data securely
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $insert_sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    $insert_stmt = $conn->prepare($insert_sql);
+    $insert_stmt->bind_param("sss", $username, $email,$hashed_password);
+
+    if ($insert_stmt->execute()) {
+        header("Location: /COOK/MENU/index.html");
+        exit;
+    } else {
+        echo "Error registering user!";
+    }
+
+    $insert_stmt->close(); // Close the insert statement
+    $username_check_stmt->close(); // Close the username check statement
+    $conn->close(); // Close the database connection
+}
+?>
